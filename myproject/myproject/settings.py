a61a5ca19ca8ad19a526d6d62376c966a995eb3e
honhaochen <haochenhon@hotmail.com>
2022-12-22 18:42:22 +0800

    setup redis

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -63,6 +63,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
+# Cache
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379',
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
